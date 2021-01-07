b44dc4b067a0b13e5ded01b45b755ea34d71bd17
fvendrasco <felipe@vendrasco.com.br>
2021-01-07 15:54:55 -0300

    Tela Home

---
+++ VendrascoMovie/Home/Model/ImportaDados.swift
@@ -0,0 +1,13 @@
+//
+//  ImportaDados.swift
+//  VendrascoMovie
+//
+//  Created by Felipe Augusto Vendrasco on 07/01/21.
+//  Copyright © 2021 Felipe Augusto Vendrasco. All rights reserved.
+//
+
+import UIKit
+
+class ImportaDados: NSObject {
+
+}
