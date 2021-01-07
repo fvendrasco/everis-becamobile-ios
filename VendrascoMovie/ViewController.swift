7e535298084f127020f94bc4c147517211cc49b6
fvendrasco <felipe@vendrasco.com.br>
2021-01-07 14:15:57 -0300

    Initial Commit

---
+++ VendrascoMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VendrascoMovie
+//
+//  Created by Felipe Augusto Vendrasco on 07/01/21.
+//  Copyright © 2021 Felipe Augusto Vendrasco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
