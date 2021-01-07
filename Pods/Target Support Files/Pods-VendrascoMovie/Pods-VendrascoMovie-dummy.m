774b96e644691617a392b58c3377f6ca89396065
fvendrasco <felipe@vendrasco.com.br>
2021-01-07 18:52:27 -0300

    Retorno API + Tela

---
+++ Pods/Target Support Files/Pods-VendrascoMovie/Pods-VendrascoMovie-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VendrascoMovie : NSObject
+@end
+@implementation PodsDummy_Pods_VendrascoMovie
+@end
--- VendrascoMovie/ViewController.swift
+++ VendrascoMovie/ViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+import Foundation
+import Alamofire
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tabelaFilmes: UITableView!
+    
+    let listaFilmes:Array<String> = ["The Mandalorian", "Mulher Maravilha", "Soul"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tabelaFilmes.dataSource = self
+        importaListaDeFilmes()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // MARK: - GET API
+    
+    func importaListaDeFilmes() {
+        Alamofire.request("https://api.themoviedb.org/3/trending/all/week?api_key=d30569abd40d52ac10aae81269a39d4d&language=pt-BR", method: .get).responseJSON { (response) in
+            
+            switch response.result{
+            
+            case .success:
+                
+                if let resposta = response.result.value as? Dictionary<String, Any> {
+                    guard let filmesLista = resposta["results"] as? Array<Dictionary<String, Any>> else {return}
+                }
+                
+                break
+            case .failure:
+                print(response.error!)
+                
+                break
+                
+            }
+        }
+    }
+    
+    
+    // MARK: Tela Home
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listaFilmes.count
+    }
+ 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = tableView.dequeueReusableCell(withIdentifier: "celula", for: indexPath)
+        celula.textLabel?.text = listaFilmes[indexPath.row]
+        
+        return celula
+    }
 
+
+    // MARK: Tela Descricao
+    
+    
 }
 
