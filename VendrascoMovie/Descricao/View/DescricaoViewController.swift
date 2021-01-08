32a7a00e2fd393949c02b453d104be6627b86032
fvendrasco <felipe@vendrasco.com.br>
2021-01-08 17:43:22 -0300

    ADD segunda tela Descricao

---
+++ VendrascoMovie/Descricao/View/DescricaoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DescricaoViewController.swift
+//  VendrascoMovie
+//
+//  Created by Felipe Augusto Vendrasco on 08/01/21.
+//  Copyright © 2021 Felipe Augusto Vendrasco. All rights reserved.
+//
+
+import UIKit
+
+class DescricaoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
